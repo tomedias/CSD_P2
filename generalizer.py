bfe84b096a2d2da1088eaf2195eb379a33932eda
Tomé Dias <tomedias1711@gmail.com>
2024-06-08 14:15:24 +0200

    Minor corrections by Vasco

--- generalizer.py
+++ generalizer.py
@@ -5,33 +5,35 @@ import matplotlib.pyplot as plt
 
 
 def generalize_year(date_str):
-    day, month, year = date_str.split('/')
+    day, month, year = date_str.split("/")
     generalized_date = f"00/{month}/{year}"
-    new_year= round(int(year) / 10) * 10
+    new_year = round(int(year) / 10) * 10
     generalized_date = generalized_date.replace(year, str(new_year))
 
     return generalized_date
 
+
 def generalize_year2(date_str):
-    day, month, year = date_str.split('/')
+    day, month, year = date_str.split("/")
     generalized_date = f"00/{month}/{year}"
     new_year = round(int(year) / 50) * 50
     generalized_date = generalized_date.replace(year, str(new_year))
 
     return generalized_date
 
+
 def generalize_month(date_str):
-    day, month, year = date_str.split('/')
+    day, month, year = date_str.split("/")
     generalized_date = f"00/{month}/{year}"
     new_month = old_month = int(month)
     if 1 <= old_month <= 3:
-        new_month = '01'
+        new_month = "01"
     elif 4 <= old_month <= 6:
-        new_month = '02'
+        new_month = "02"
     elif 7 <= old_month <= 9:
-        new_month = '03'
+        new_month = "03"
     elif 10 <= old_month <= 12:
-        new_month = '04'
+        new_month = "04"
     generalized_date = generalized_date.replace(month, str(new_month))
 
     return generalized_date
@@ -45,14 +47,15 @@ def generalize_education(status):
     else:
         return 2
 
-def generalize(df):
-    df['Education Status'] = df['Education Status'].astype(int)
-    df['Education Status'] = df['Education Status'].apply(generalize_education)
 
-    df['Date of Birth'] = df['Date of Birth'].apply(generalize_year2)
-    df['Date of Birth'] = df['Date of Birth'].apply(generalize_month)
-    df['Postal Code'] = df['Postal Code'].astype(str)
-    df['Postal Code'] = df['Postal Code'].apply(lambda x: x[:2] + 'XX')
+def generalize(df):
+    df["Education Status"] = df["Education Status"].astype(int)
+    df["Education Status"] = df["Education Status"].apply(generalize_education)
+
+    df["Date of Birth"] = df["Date of Birth"].apply(generalize_year2)
+    df["Date of Birth"] = df["Date of Birth"].apply(generalize_month)
+    df["Postal Code"] = df["Postal Code"].astype(str)
+    df["Postal Code"] = df["Postal Code"].apply(lambda x: x[:2] + "XX")
 
 
 data = pd.read_csv("sti_data.csv")
--- k-anon.py
+++ k-anon.py
@@ -1,21 +1,23 @@
+import argparse
 import pandas as pd
 
+
 def isKAnonymized(df, k):
-    quasi_ids = ['Date of Birth', 'Postal Code', 'Education Status']
+    quasi_ids = ["Date of Birth", "Postal Code", "Education Status"]
     unique_entries = []
-    for index, row in df.iterrows():
+    for _, row in df.iterrows():
         query_parts = []
         for col in quasi_ids:
             value = row[col]
             if pd.isna(value):
-                query_parts.append(f'`{col}`.isna()')
+                query_parts.append(f"`{col}`.isna()")
             elif isinstance(value, str):
                 query_parts.append(f'`{col}` == "{value}"')
             elif isinstance(value, bool):
-                query_parts.append(f'`{col}` == {value}')
+                query_parts.append(f"`{col}` == {value}")
             else:
-                query_parts.append(f'`{col}` == {value}')
-        query = ' & '.join(query_parts)
+                query_parts.append(f"`{col}` == {value}")
+        query = " & ".join(query_parts)
         try:
             rows = df.query(query)
         except Exception as e:
@@ -32,10 +34,24 @@ def isKAnonymized(df, k):
     return True
 
 
-data = pd.read_csv("sti_data_anon.csv")
-print(data.columns)
+def main(data_file, k_value):
+    data = pd.read_csv(data_file)
+    print(data.columns)
 
-if isKAnonymized(data, 5):
-      print("All good")
-else:
-    print("Womp Womp")
+    if isKAnonymized(data, int(k_value)):
+        print("All good")
+    else:
+        print("Womp Womp")
+
+
+if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="K-Anonymizer.")
+    parser.add_argument("original_file", help="Path to the data file to anonymize.")
+    parser.add_argument("k_value")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Call main function with parsed arguments
+    main(args.original_file, args.k_value)
