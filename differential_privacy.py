68acaabfb3ae62229718a1ec36bd699505a99dcf
Tomé Dias <tomedias1711@gmail.com>
2024-06-08 18:12:06 +0200

    Some Cooking

--- differential_privacy.py
+++ differential_privacy.py
@@ -1,31 +1,58 @@
-import pandas as pd
 import argparse
-import sqlite3
 import os
 import numpy as np
 import re
+import pandas as pd
+import sqlite3
+from datetime import datetime
 
 # We chose a low value of 1 for the privacy budget to guarantee strong privacy due to the sensitivity of the data (STIs)
 EPSILON = 1
 
+DATE_FORMAT = "%d/%m/%Y"
+
+
+class Sensitivity:
+    POSTAL_CODE_SENSITIVITY = None
+    EDUCATION_STATUS_SENSITIVITY = None
+    YEAR_SENSITIVITY = 99  # 2049 - 1950
+    DAY_SENSITIVITY = 31
+    MONTH_SENSITIVITY = 12
+    DECEASE_SENSITIVITY = 1
+
+
+def calculate_sensitivity(data):
+    return data.max() - data.min()
+
 
 def date_of_birth(date):
-    return 1
+    date_object = datetime.strptime(date, DATE_FORMAT)
+    new_day = laplace_mech(date_object.day, Sensitivity.DAY_SENSITIVITY, EPSILON)
+    new_month = laplace_mech(date_object.month, Sensitivity.MONTH_SENSITIVITY, EPSILON)
+    new_year = laplace_mech(date_object.year, Sensitivity.YEAR_SENSITIVITY, EPSILON)
+    return "{}/{}/{}".format(int(new_day), int(new_month), int(new_year))
+
 
 def post_code(postal_code):
-    return 2
+    return laplace_mech(postal_code, Sensitivity.POSTAL_CODE_SENSITIVITY, EPSILON)
+
 
 def education_status(education_status):
-    return 3
+    return laplace_mech(
+        education_status, Sensitivity.EDUCATION_STATUS_SENSITIVITY, EPSILON
+    )
+
 
 def chlamydia(chlamydia):
-    return laplace_mech(chlamydia, 1, 0.1)
+    return laplace_mech(chlamydia, Sensitivity.DECEASE_SENSITIVITY, 0.1)
+
 
 def gonorrhea(gonorrhea):
-    return laplace_mech(gonorrhea, 1, 0.1)
+    return laplace_mech(gonorrhea, Sensitivity.DECEASE_SENSITIVITY, 0.1)
+
 
 def syphilis(syphilis):
-    return laplace_mech(syphilis, 1, 0.1)
+    return laplace_mech(syphilis, Sensitivity.DECEASE_SENSITIVITY, 0.1)
 
 
 function_map = {
@@ -95,17 +122,25 @@ def execute_commands(conn):
 
 def main(sti_data):
     data = pd.read_csv(sti_data)
+    Sensitivity.POSTAL_CODE_SENSITIVITY = calculate_sensitivity(data["Postal Code"])
+    Sensitivity.EDUCATION_STATUS_SENSITIVITY = calculate_sensitivity(
+        data["Education Status"]
+    )
     with sqlite3.connect("sti_data.db") as conn:
         data.to_sql("sti", conn, if_exists="replace", index=False)
         execute_commands(conn)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Differential Privacy.")
-    parser.add_argument(
-        "sti_data",
-        help="Path to the sti_data data file.",
-    )
-    args = parser.parse_args()
-    main(args.sti_data)
-    os.remove("sti_data.db")
+    try:
+        parser = argparse.ArgumentParser(description="Differential Privacy.")
+        parser.add_argument(
+            "sti_data",
+            help="Path to the sti_data data file.",
+        )
+        args = parser.parse_args()
+        main(args.sti_data)
+        os.remove("sti_data.db")
+    except Exception as e:
+        print(e)
+        os.remove("sti_data.db")
