d07b88cf6cc2dff3d5cca9a511d9dd3bb02491ed
Tomé Dias <tomedias1711@gmail.com>
2024-06-08 21:00:19 +0200

    Defined dataset clipping and fixed SENSITIVITIES, increasing count epsilon and stripped strings.

--- differential_privacy.py
+++ differential_privacy.py
@@ -8,55 +8,42 @@ from datetime import datetime
 
 # We chose a low value of 1 for the privacy budget to guarantee strong privacy due to the sensitivity of the data (STIs)
 EPSILON = 1.0
-
 DATE_FORMAT = "%d/%m/%Y"
 
 
 class Sensitivity:
-    POSTAL_CODE_SENSITIVITY = None
-    EDUCATION_STATUS_SENSITIVITY = None
+    POSTAL_CODE_SENSITIVITY = 3000
+    EDUCATION_STATUS_SENSITIVITY = 16
     YEAR_SENSITIVITY = 99  # 2049 - 1950
     DAY_SENSITIVITY = 30  # 31 - 1
     MONTH_SENSITIVITY = 11  # 12 - 1
     DECEASE_SENSITIVITY = 1  # 1 - 0
 
+
 def expended_budget(epsilon):
     global EPSILON
-    if(EPSILON <= 0.0):
+    if EPSILON <= 0.0:
         raise Exception("Budget exceeded")
-    if((EPSILON - epsilon) < 0.0):
+    if (EPSILON - epsilon) < 0.0:
         raise Exception("Budget exceeded")
+
 
 def calculate_sensitivity(data):
     return data.max() - data.min()
+
 
 def laplace_mech(v, sensitivity, epsilon):
     global EPSILON
     expended_budget(epsilon)
     EPSILON -= epsilon
     return v + np.random.laplace(loc=0, scale=sensitivity / epsilon)
-    
+
 
 def date_of_birth(date):
     date_object = datetime.strptime(date, DATE_FORMAT)
-    day = 0 if date_object.day < 0 else 31 if date_object.day > 31 else date_object.day
-    month = (
-        1
-        if date_object.month < 1
-        else 12
-        if date_object.month > 12
-        else date_object.month
-    )
-    year = (
-        1950
-        if date_object.year < 1950
-        else 2049
-        if date_object.year > 2049
-        else date_object.year
-    )
-    new_day = laplace_mech(day < 0, Sensitivity.DAY_SENSITIVITY, 1)
-    new_month = laplace_mech(month, Sensitivity.MONTH_SENSITIVITY, 1)
-    new_year = laplace_mech(year, Sensitivity.YEAR_SENSITIVITY, 1)
+    new_day = laplace_mech(date_object.day < 0, Sensitivity.DAY_SENSITIVITY, 1)
+    new_month = laplace_mech(date_object.month, Sensitivity.MONTH_SENSITIVITY, 1)
+    new_year = laplace_mech(date_object.year, Sensitivity.YEAR_SENSITIVITY, 1)
     return "{}/{}/{}".format(int(new_day), int(new_month), int(new_year))
 
 
@@ -65,12 +52,12 @@ def post_code(postal_code):
 
 
 def education_status(education_status):
-    return laplace_mech(
-        education_status, Sensitivity.EDUCATION_STATUS_SENSITIVITY, 1)
+    return laplace_mech(education_status, Sensitivity.EDUCATION_STATUS_SENSITIVITY, 1)
 
 
 def chlamydia(chlamydia):
     return laplace_mech(chlamydia, Sensitivity.DECEASE_SENSITIVITY, 0.1)
+
 
 def gonorrhea(gonorrhea):
     return laplace_mech(gonorrhea, Sensitivity.DECEASE_SENSITIVITY, 0.1)
@@ -93,19 +80,19 @@ def sum_postal_code(value):
 
 
 def count(value):
-    return laplace_mech(value, 1, 0.1)
+    return laplace_mech(value, 1, 0.3)
 
 
 def avg_decease(sum, count_value):
     return sum_decease(sum) / count(count_value)
 
 
-def avg_education_status(sum, count):
-    return sum_education_status(sum) / count(count)
+def avg_education_status(sum, count_value):
+    return sum_education_status(sum) / count(count_value)
 
 
-def avg_postal_code(sum, count):
-    return sum_postal_code(sum) / count(count)
+def avg_postal_code(sum, count_value):
+    return sum_postal_code(sum) / count(count_value)
 
 
 function_map = {
@@ -128,13 +115,13 @@ function_map = {
 def create_sum_function(sum_fields, function_map):
     for original in sum_fields:
         if any(
-            field.lower() in original.lower()
+            field.lower() in original.lower().strip()
             for field in ["Chlamydia", "Syphilis", "Gonorrhea"]
         ):
             function_map[original] = sum_decease
-        elif "Education Status".lower() in original.lower():
+        elif "Education Status".lower() in original.lower().strip():
             function_map[original] = sum_education_status
-        elif "Postal Code".lower() in original.lower():
+        elif "Postal Code".lower() in original.lower().strip():
             function_map[original] = sum_postal_code
 
 
@@ -146,13 +133,13 @@ def create_count_function(count_fields, function_map):
 def create_avg_function(avg_fields, function_map):
     for original in avg_fields:
         if any(
-            field.lower() in original.lower()
+            field.lower() in original.lower().strip()
             for field in ["Chlamydia", "Syphilis", "Gonorrhea"]
         ):
             function_map[original] = avg_decease
-        elif "Education Status".lower() in original.lower():
+        elif "Education Status".lower() in original.lower().strip():
             function_map[original] = avg_education_status
-        elif "Postal Code".lower() in avg_fields.lower():
+        elif "Postal Code".lower() in avg_fields.lower().strip():
             function_map[original] = avg_postal_code
 
 
@@ -160,9 +147,10 @@ def create_function_map(renamed_fields):
     new_function_map = function_map.copy()
     for original, new in renamed_fields:
         new_function_map[new] = new_function_map[
-            original.replace('"', "").replace("'", "")
+            original.replace('"', "").replace("'", "").strip()
         ]
     return new_function_map
+
 
 def create_dynamic_class_instance(class_name, field_names, row):
     class_obj = type(class_name, (object,), {})
@@ -204,7 +192,13 @@ def execute_query(cursor, query: str):
         for field in fields:
             if "avg" in field.lower():
                 new_field = (
-                    field.lower().replace("avg", "").replace("(", "").replace(")", "")
+                    field.lower()
+                    .replace("avg", "")
+                    .replace("(", "")
+                    .replace(")", "")
+                    .replace('"', "")
+                    .replace("'", "")
+                    .strip()
                 )
                 cursor.execute(
                     "SELECT SUM({}), COUNT({}) FROM {}".format(
@@ -231,14 +225,25 @@ def execute_commands(conn):
         command = input()
 
 
+def clip_dataset(data):
+    data["Date of Birth"] = pd.to_datetime(
+        data["Date of Birth"], format=DATE_FORMAT, errors="coerce"
+    )
+    lower_bound = pd.to_datetime("01/01/1950", format="%d/%m/%Y")
+    upper_bound = pd.to_datetime("31/12/2049", format="%d/%m/%Y")
+    data["Date of Birth"] = data["Date of Birth"].clip(
+        lower=lower_bound, upper=upper_bound
+    )
+    data["Date of Birth"] = data["Date of Birth"].dt.strftime("%d/%m/%Y")
+    data["Education Status"] = data["Education Status"].clip(lower=0, upper=16)
+    data["Postal Code"] = data["Postal Code"].clip(lower=0, upper=3000)
+
+
 def main(sti_data):
     try:
         data = pd.read_csv(sti_data)
-        Sensitivity.POSTAL_CODE_SENSITIVITY = calculate_sensitivity(data["Postal Code"])
-        Sensitivity.EDUCATION_STATUS_SENSITIVITY = calculate_sensitivity(
-            data["Education Status"]
-        )
-        conn =  sqlite3.connect("sti_data.db")
+        clip_dataset(data)
+        conn = sqlite3.connect("sti_data.db")
         data.to_sql("sti", conn, if_exists="replace", index=False)
         execute_commands(conn)
     except Exception as e:
@@ -256,4 +261,3 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     main(args.sti_data)
-    
